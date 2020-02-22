9f060c1134eb6d7cfcd51fb4176dc9a55f73f8e9
archanshahh <archanshahh@gmail.com>
2020-02-22 10:29:22 -0500

    app done

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -23,10 +23,15 @@ export default class CreateExercise extends Component {
     }
 
     componentDidMount(){
-        this.setState({
-            users: ['test users'],
-            username: 'test user'
-        })
+        axios.get('http://localhost:5000/users/')
+            .then(res => {
+                if(res.data.length > 0){
+                    this.setState({
+                        users: res.data.map(user=> user.username),
+                        username: res.data[0].username
+                    })
+                }
+            });
     }
     onChangeUsername(e){
         this.setState({
@@ -63,6 +68,9 @@ export default class CreateExercise extends Component {
           }
 
           console.log(exercise)
+
+          axios.post('http://localhost:5000/exercises/add', exercise)
+            .then(res => console.log(res.data));
 
           window.location = "/";
       }
